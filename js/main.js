b44e8069eb4bafe2dd43fbff78d1658d3e25a459
Devmdmainuddin <devmdmainuddin@gmail.com>
2024-10-16 12:15:36 +0600

    product details page design

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ toggleOpen.addEventListener('click', handleClick);
 toggleClose.addEventListener('click', handleClick);
 
 
-
+// slider Herosections
 
 var swiper = new Swiper(".heroSwiper", {
     slidesPerView: 1,
@@ -25,9 +25,9 @@ var swiper = new Swiper(".heroSwiper", {
         delay: 2500,
         disableOnInteraction: false,
     },
-   
-});
 
+});
+// slider testimonials
 var swiper = new Swiper(".testimonialSwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
@@ -45,17 +45,116 @@ var swiper = new Swiper(".testimonialSwiper", {
         0: {
             slidesPerView: 1,
             spaceBetween: 18,
-            
+
         },
         768: {
             slidesPerView: 2,
             spaceBetween: 20,
-            
+
         },
         1024: {
             slidesPerView: 3,
             spaceBetween: 32,
         },
     },
-   
+
 });
+
+// API calls
+
+const getData = () => {
+    fetch("https://dummyjson.com/products")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.products);
+            displayProducts(data.products);
+            displayCategory(data.products)
+        })
+}
+getData()
+
+// const displayProducts = (products) => {
+
+
+//     products.forEach(product=> {
+//         const productsContainer = document.getElementById('products');
+//         productsContainer.innerHTML += `
+        
+//                     <div
+//                         class="bg-gray-100 p-3 rounded-lg group overflow-hidden cursor-pointer relative z-50 hover:before:bg-black before:absolute before:inset-0 before:opacity-20 before:transition-all">
+//                         <div class="w-full h-[300px] overflow-hidden mx-auto aspect-w-16 aspect-h-8">
+//                             <img src=${product?.images[0]} alt="product1"
+//                                 class="h-full w-full object-contain" />
+//                         </div>
+    
+//                         <div
+//                             class="absolute mx-auto left-0 right-0 -bottom-80 group-hover:bottom-2 bg-white w-11/12 p-3 rounded-lg transition-all duration-500">
+//                             <div class="text-center">
+//                                 <h3 class="text-base font-bold text-gray-800">${product.title}</h3>
+//                                 <h4 class="text-lg text-blue-600 font-bold mt-2">$${product.price}</h4>
+//                             </div>
+    
+//                             <div class="flex justify-center space-x-1 mt-4 text-[#facc15]">
+//                                 <i class="fa-solid fa-star"></i>
+//                                 <i class="fa-solid fa-star"></i>
+//                                 <i class="fa-solid fa-star"></i>
+//                                 <i class="fa-solid fa-star"></i>
+//                                 <i class="fa-solid fa-star text-[#CED5D8]"></i>
+//                             </div>
+//                         </div>
+//                     </div>
+//         `
+        
+//     })
+   
+// }
+
+
+const displayProducts = (products) => {
+    const productsContainer = document.getElementById('products');
+
+    products.forEach(product => {
+        productsContainer.innerHTML += `
+            <div
+                class="bg-gray-100 p-3 rounded-lg group overflow-hidden cursor-pointer relative z-50 hover:before:bg-black before:absolute before:inset-0 before:opacity-20 before:transition-all"
+                onclick="window.location.href='product-details.html?id=${product.id}'">  <!-- Link to product details page -->
+                <div class="w-full h-[300px] overflow-hidden mx-auto aspect-w-16 aspect-h-8">
+                    <img src=${product?.images[0]} alt="product1" class="h-full w-full object-contain" />
+                </div>
+
+                <div
+                    class="absolute mx-auto left-0 right-0 -bottom-80 group-hover:bottom-2 bg-white w-11/12 p-3 rounded-lg transition-all duration-500">
+                    <div class="text-center">
+                        <h3 class="text-base font-bold text-gray-800">${product.title}</h3>
+                        <h4 class="text-lg text-blue-600 font-bold mt-2">$${product.price}</h4>
+                    </div>
+                    
+                    <div class="flex justify-center space-x-1 mt-4 text-[#facc15]">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star text-[#CED5D8]"></i>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+}
+
+
+
+
+
+
+
+
+
+const displayCategory = (products) => {
+
+   const category= [...new Set(products?.map(product=>product.category))]
+console.log(category);
+    
+   
+}
+displayCategory()
